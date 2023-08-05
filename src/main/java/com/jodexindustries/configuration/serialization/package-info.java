5b5286500efa379ad9543e9e0ec5e9e86fa5bf81
JodexIndustries <0984134q@gmail.com>
2023-08-05 15:44:49 +0300

    1.0.0

---
+++ src/main/java/com/jodexindustries/configuration/serialization/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Classes dedicated to being able to perform serialization specialized for
+ * the Bukkit {@link com.jodexindustries.configuration.Configuration configuration}
+ * implementation.
+ */
+package com.jodexindustries.configuration.serialization;
+
